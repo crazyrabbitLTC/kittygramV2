bd9da8de3ad014d8c2d5695777ea89c4215646cb
Dennison Bertram <dennison@dennisonbertram.com>
2019-05-25 15:41:45 -0400

    list kitties

--- client/src/components/KittyContainer/index.js
+++ client/src/components/KittyContainer/index.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 import { PublicAddress, Blockie } from 'rimble-ui';
 import styles from './KittyContainer.module.scss';
+import Axios from "axios";
 
 export default class KittyContainer extends Component {
-
+  constructor(props){
+    super(props);
+    this.state = {
+      openSea: "https://api.opensea.io/api/v1/assets?",
+      cats: []
+  }
+}
   componentDidMount = async () => {
-    
+    const { networkId, accounts, balance, isMetaMask } = this.props;
+    const {openSea} = this.state;
+    const asset_contract_address = "0x06012c8cf97bead5deae237070f9587f8e7a266d";
+    console.log("The props: ", this.props);
+
+    if(networkId && accounts && isMetaMask){
+      console.log("ready to go");
+
+      const queryAddress = `${openSea}owner=${accounts[0]}&asset_contract_address=${asset_contract_address}`;
+
+      const res = await Axios.get(queryAddress);
+      console.log(res.data.assets);
+      this.setState({...this.state, cats: res.data.assets});
+    } else (
+      console.log("Not this render")
+    )
+
+
   }
 
   renderNetworkName(networkId) {
@@ -25,11 +49,14 @@ export default class KittyContainer extends Component {
 
   render()  {
     const { networkId, accounts, balance, isMetaMask } = this.props;
-    console.log(`The Props: `);
-    console.dir(this.props);
+    const {cats} = this.state;
+    const allCats = cats.map((cat)=> <div><img src={`${cat.image_preview_url}`} alt="boohoo" className="img-responsive"/> </div>)
     return (
       <div className={styles.web3}>
         <h3> Your Web3 Info </h3>
+        Number of Cats: {cats.length}
+        <div>{allCats}</div>
+        
         <div className={styles.dataPoint}>
           <div className={styles.label}>
             Network:
